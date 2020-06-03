cf6e69c5dba4315ddbc5d4ed28cb761396ce9ec5
Gisleine <gisleine.moreira@gmail.com>
2020-06-03 16:12:58 -0300

    consegui corrigir o erro do javascript

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+
+
+var nome = "Gisleine Moreira";
+var idade = 29;
+var idade2 = 10;
+var frase = "japão é o melhor time!";
+//alert(nome + "tem" + idade + "anos");
+
+//alert(idade + idade2);
+console.log("oie");
+console.log(name);
+console.log(idade + idade2);
