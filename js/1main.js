a76cc407a23e87c1e6329344c60b9bf1ab9de5c7
Gisleine <gisleine.moreira@gmail.com>
2020-06-03 22:37:44 -0300

    adicionei lista dos estados e cidades

--- js/1main.js
+++ js/1main.js
@@ -9,7 +9,7 @@ function populateUFs() {
     .then( states => {
 
         for( const state of states) {
-            ufSelect .innerHTML +=  `<option value="${state.id}">${state.nome}</option>`            
+            ufSelect.innerHTML +=  `<option value="${state.id}">${state.nome}</option>`            
         }
 
         
@@ -17,7 +17,35 @@ function populateUFs() {
 }
 
 populateUFs()
-document.querySelector("select[name=uf]")
-     document.addEventListener("change", () => {
-        console.log("mudei")
+
+function getCities(event) {
+    const citySelect = document.querySelector("select[name=city]")
+    const stateInput = document.querySelector("input[name=state]")
+
+    const ufValue = event.target.value
+    
+    const indexOfSelectadState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectadState].text
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+    fetch(url)
+    .then( res =>  res.json() )
+    .then( cities => {
+
+        for( const city of cities) {
+            citySelect.innerHTML +=  `<option value="${city.id}">${city.nome}</option>`
+
+        }
+
+        citySelect.disabled = false     
     } )
+
+}
+
+
+
+
+document
+     .querySelector("select[name=uf]")
+     .addEventListener("change", getCities)
